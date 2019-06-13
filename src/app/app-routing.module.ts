457e7bb8985c3d4d3313d20a8f3f0be4f9d32b3e
geosh2000 <geosh2000@gmail.com>
2019-06-13 13:52:07 -0500

    Inicio y detalle asesores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { DetalleAsesoresComponent } from './components/hc/detalle-asesores/detalle-asesores.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  { path: 'home', component: HomeComponent },
+  { path: 'detalle-asesores', component: DetalleAsesoresComponent },
+  { path: 'detalle-asesores/:id', component: DetalleAsesoresComponent },
+  { path: 'detalle-asesores/:id/:tipo', component: DetalleAsesoresComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,234 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+
+//Services
+import { NavbarService, AsesoresService, LoginService, CredentialsService, TokenCheckService, ApiService, InitService, GlobalServicesService } from './services/service.index';
+
+
+//Components
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { HomeComponent } from './components/home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { UiSwitchModule } from 'ngx-ui-switch';
+import { OrderModule } from 'ngx-order-pipe';
+import { MultiselectDropdownModule } from 'angular-2-dropdown-multiselect';
+import { RouterModule } from '@angular/router';
+import { Ng2CompleterModule } from 'ng2-completer';
+import { CommonModule } from '@angular/common';
+
+// Pipes
+import { KeysPipe } from './pipes/keys.pipe';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { NoAcentosPipe } from './pipes/no-acentos.pipe';
+
+// Components
+import { AsistenciaBadgeComponent } from './shared/buttons/asistencia-badge/asistencia-badge.component';
+import { BonoApproveComponent } from './shared/buttons/bono-approve/bono-approve.component';
+import { ExtraSwitchComponent } from './shared/buttons/extra-switch/extra-switch.component';
+import { PuntualidadBadgeComponent } from './shared/buttons/puntualidad-badge/puntualidad-badge.component';
+import { SaBadgeComponent } from './shared/buttons/sa-badge/sa-badge.component';
+import { LoginComponent } from './shared/login/login.component';
+import { LogoutComponent } from './shared/logout/logout.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { PbxStatusComponent } from './shared/pbx-status/pbx-status.component';
+import { SearchAsesorComponent } from './shared/search-asesor/search-asesor.component';
+import { TableTemplateComponent } from './shared/table-template/table-template.component';
+import { CsvComponent } from './shared/upload/csv/csv.component';
+import { CumplimientoComponent } from './shared/progress/cumplimiento/cumplimiento.component';
+import { AsesorFilterComponent } from './shared/filters/asesor-filter/asesor-filter.component';
+import { UserPreferencesComponent } from './shared/user-preferences/user-preferences.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { PdvBadgeComponent } from './shared/buttons/pdv-badge/pdv-badge.component';
+import { DetalleAsesoresComponent } from './components/hc/detalle-asesores/detalle-asesores.component';
+import { DetVacacionesComponent } from './components/hc/detalle-asesores/det-vacaciones/det-vacaciones.component';
+import { DetDiasPendientesComponent } from './components/hc/detalle-asesores/det-dias-pendientes/det-dias-pendientes.component';
+import { DetDetalleComponent } from './components/hc/detalle-asesores/det-detalle/det-detalle.component';
+import { DetContratoComponent } from './components/hc/detalle-asesores/det-contrato/det-contrato.component';
+import { DetSalarioComponent } from './components/hc/detalle-asesores/det-salario/det-salario.component';
+import { DetHistorialComponent } from './components/hc/detalle-asesores/det-historial/det-historial.component';
+import { DetHorarioComponent } from './components/hc/detalle-asesores/det-horario/det-horario.component';
+import { UploadImageComponent } from './components/formularios/upload-image.component';
+import { AddAusentismoComponent } from './components/formularios/add-ausentismo.component';
+import { PyaExceptionComponent } from './components/formularios/pya-exception.component';
+import { EditDetailsComponent } from './components/formularios/edit-details.component';
+import { SetBajaComponent } from './components/formularios/set-baja.component';
+import { AddContratoComponent } from './components/formularios/add-contrato.component';
+import { ReingresoAsesorComponent } from './components/formularios/reingreso-asesor.component';
+import { CambioPuestoComponent } from './components/formularios/cambio-puesto.component';
+import { JornadasComponent } from './components/reportes/asistencia/jornadas.component';
+import { Daterangepicker } from 'ng2-daterangepicker';
+import { PopoverModule } from 'ngx-popover';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+
+    // Pipes
+    KeysPipe,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    NoAcentosPipe,
+
+    //Components
+    AsistenciaBadgeComponent,
+    BonoApproveComponent,
+    ExtraSwitchComponent,
+    PuntualidadBadgeComponent,
+    SaBadgeComponent,
+    AsesorFilterComponent,
+    LoginComponent,
+    LogoutComponent,
+    NavbarComponent,
+    PbxStatusComponent,
+    CumplimientoComponent,
+    SearchAsesorComponent,
+    TableTemplateComponent,
+    CsvComponent,
+    UserPreferencesComponent,
+    SidebarComponent,
+    PdvBadgeComponent,
+    DetalleAsesoresComponent,
+    DetVacacionesComponent,
+    DetDiasPendientesComponent,
+    DetDetalleComponent,
+    DetContratoComponent,
+    DetSalarioComponent,
+    DetHistorialComponent,
+    DetHorarioComponent,
+    UploadImageComponent,
+    AddAusentismoComponent,
+    PyaExceptionComponent,
+    EditDetailsComponent,
+    SetBajaComponent,
+    AddContratoComponent,
+    ReingresoAsesorComponent,
+    CambioPuestoComponent,
+    JornadasComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    CommonModule,
+
+    BrowserAnimationsModule,
+    HttpClientModule,
+    Ng2CompleterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    UiSwitchModule,
+    NgbModule.forRoot(),
+    ToastrModule.forRoot(),
+    OrderModule,
+    MultiselectDropdownModule,
+    Daterangepicker,
+    PopoverModule,
+    
+
+    MatButtonModule, MatCheckboxModule,
+
+    // ==================================================
+    // START ANGULAR MATERIAL
+    // ==================================================
+    CdkTableModule,
+    CdkTreeModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    // ==================================================
+    // END ANGULAR MATERIAL
+    // ==================================================
+
   ],
-  providers: [],
+  providers: [
+    NavbarService,
+    AsesoresService,
+    LoginService,
+    CredentialsService,
+    TokenCheckService,
+    ApiService,
+    InitService,
+    GlobalServicesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
